2b7f75002e915a653078d5e75a38f3ebc5e9ae00
UriProtocol <angel_3041210085@utd.edu.mx>
2022-11-29 11:51:36 -0600

    v5.1

--- index.php
+++ index.php
@@ -11,11 +11,7 @@
     <h1>soy el index</h1>
     <h1>soy el index modificado desde git Hub remoto</h1>
     <h2>esta modificacion es desde VScode local.  jhghjghjghj</h2>
-<<<<<<< HEAD
     <h3>Buenos días <b>profe</b></h3>
-=======
-
     <h1>Hola soy Juan Pablo</h1>
->>>>>>> 02e891a04c763d3028e677a922deb591451e3c1e
 </body>
 </html>
